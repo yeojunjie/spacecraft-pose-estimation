3037022bcc6ea3b141d5903220df9f67bad8d88d
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-03-20 21:17:15 +0800

    2 cnn models, but they are slow to test

---
+++ models/ivan-simple-cnn/main.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DATA_DIR=../../data
+SUBMISSION_PATH=../../data/submission/submission.csv
+
+# call our script (main.py in this case) and tell it where the data and submission live
+python3 main.py $DATA_DIR $SUBMISSION_PATH
